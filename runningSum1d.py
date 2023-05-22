60ac88849a3d86b23e55c12c983e28f3687c3a18
Lukas <12475275+Lukas-Forst@users.noreply.github.com>
2023-05-22 20:27:52 +0200

    Create runningSum1d.py

---
+++ runningSum1d.py
@@ -0,0 +1,12 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        running_sum = 0
+        result = []
+        
+        # Iterate through the input list
+        for num in nums:
+            # Add the current number to the running sum
+            running_sum += num
+            # Append the running sum to the result list
+        
+        return result
